f816f696191ca9e705a7bc7834ffea28a9c5c63e
Dan10022002 <70635329+Dan10022002@users.noreply.github.com>
2020-12-11 22:19:12 +0300

    Update and rename work.cpp to LR9.cpp

--- LR9.cpp
+++ LR9.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
 
 template <class T>
 struct Tree_element
@@ -10,7 +12,7 @@ struct Tree_element
 };
 
 template <typename T> //6 - поиск нужного элемента
-T Find_element (Tree_element<T>& Tree, T n)
+Tree_element<T>* Find_element (Tree_element<T>* Tree, T n)
 {
     if (Tree -> value == n)
     {
@@ -41,7 +43,7 @@ T Find_element (Tree_element<T>& Tree, T n)
 }
 
 template <typename T> //6 - поиск максимального элемента
-T Find_max_element (Tree_element<T>& Tree)
+Tree_element<T>* Find_max_element (Tree_element<T>* Tree)
 {
     if (Tree -> tree_next_right == nullptr)
     {
@@ -54,7 +56,7 @@ T Find_max_element (Tree_element<T>& Tree)
 }
 
 template <typename T> //6 - поиск минимального элемента
-T Find_min_element (Tree_element<T>& Tree)
+Tree_element<T>* Find_min_element (Tree_element<T>* Tree)
 {
     if (Tree -> tree_next_left == nullptr)
     {
@@ -67,7 +69,7 @@ T Find_min_element (Tree_element<T>& Tree)
 }
 
 template <typename T> //6 - восстановление уровня узла
-void Fix_key (Tree_element<T>& Tree)
+void Fix_key (Tree_element<T>* Tree)
 {
     unsigned int key1 = Tree -> tree_next_left -> key;
     unsigned int key2 = Tree -> tree_next_right -> key;
@@ -82,7 +84,7 @@ void Fix_key (Tree_element<T>& Tree)
 }
 
 template <typename T> //6 - правый поворот
-T Right_rotation (Tree_element<T>& Tree)
+Tree_element<T>* Right_rotation (Tree_element<T>* Tree)
 {
     Tree_element<T>& tree_time = Tree -> tree_next_left;
     Tree -> tree_next_left = tree_time -> tree_next_right;
@@ -93,7 +95,7 @@ T Right_rotation (Tree_element<T>& Tree)
 }
 
 template <typename T> //6 - левый поворот
-T Left_rotation (Tree_element<T>& Tree)
+Tree_element<T>* Left_rotation (Tree_element<T>* Tree)
 {
     Tree_element<T>& tree_time = Tree -> tree_next_right;
     Tree -> tree_next_right = tree_time -> tree_next_left;
@@ -104,7 +106,7 @@ T Left_rotation (Tree_element<T>& Tree)
 }
 
 template <typename T> //6 - балансировка
-T Balance (Tree_element<T>& Tree)
+Tree_element<T>* Balance (Tree_element<T>* Tree)
 {
     Fix_key (Tree);
     if ((Tree -> tree_next_right -> key) - (Tree -> tree_next_left -> key) == 2)
@@ -137,16 +139,16 @@ T Balance (Tree_element<T>& Tree)
 }
 
 template <typename T> //1
-void Constructor (Tree_element<T>& Tree)
+void Constructor (Tree_element<T>* Tree)
 {
-    Tree.value = 0;
-    Tree.key = 1;
-    Tree.tree_next_left = nullptr;
-    Tree.tree_next_right = nullptr;
+    Tree -> value = 0;
+    Tree -> key = 1;
+    Tree -> tree_next_left = nullptr;
+    Tree -> tree_next_right = nullptr;
 }
 
 template <typename T> //3
-T Push (Tree_element<T>& Tree, T n)
+Tree_element<T>* Push (Tree_element<T>* Tree, T n)
 {
     if (Tree -> value > n)
     {
@@ -160,7 +162,7 @@ T Push (Tree_element<T>& Tree, T n)
 }
 
 template <typename T> //4
-T Erase (Tree_element<T>& Tree, T n)
+Tree_element<T>* Erase (Tree_element<T>* Tree, T n)
 {
     if (Find_element(Tree, n) == -1)
     {
@@ -194,21 +196,21 @@ T Erase (Tree_element<T>& Tree, T n)
 }
 
 template <typename T> //6 - удаление максимального узла
-T Delete_right_elements (Tree_element<T>& Tree)
+Tree_element<T>* Delete_right_elements (Tree_element<T>* Tree)
 {
     delete Find_max_element(Tree);
     return Balance(Tree);
 }
 
 template <typename T> //6 - удаление минимального узла
-T Delete_left_elements (Tree_element<T>& Tree)
+T* Delete_left_elements (Tree_element<T>* Tree)
 {
     delete Find_min_element(Tree);
     return Balance(Tree);
 }
 
 template <typename T> //2
-void Destructor (Tree_element<T>& Tree)
+void Destructor (Tree_element<T>* Tree)
 {
     while (Find_max_element(Tree) != Tree)
     {
@@ -222,13 +224,136 @@ void Destructor (Tree_element<T>& Tree)
 }
 
 template <typename T> //5
-void Print (Tree_element<T>& Tree)
+void Print (Tree_element<T>* Tree)
 {
+    Balance(Tree);
+    unsigned int key1 = Find_max_element(Tree) -> key;
+    unsigned int key2 = Find_min_element(Tree) -> key;
+    unsigned int key_max;
+    if (key1 >= key2)
+    {
+        key_max = key1;
+    }
+    else
+    {
+        key_max = key2;
+    }
+    unsigned int tab = static_cast<int>(pow(2, (key_max - 1)));
+     if (Tree != nullptr)
+     {
+         std::vector<T> Vec;
+         Print_help(Tree, 200, Vec, 4, key_max);
+         T print_string;
+         print_string.value = -1;
+         for (int i = key_max; i > 0; i--)
+         {
+             for (int j = 0; j < Vec.size(); j++)
+             {
+                 if (Vec[j].key == i)
+                 {
+                     for (int k = 0; k < tab; k++)
+                     {
+                         std::cout << " ";
+                     }
+                     if (Vec[j].value == -1)
+                     {
+                         std::cout << "-";
+                     }
+                     else
+                     {
+                         std::cout << Vec[j].value;
+                     }
+                     for (int k = 0; k < (tab - 1); k++)
+                     {
+                         std::cout << " ";
+                     }
+                 }
+             }
+             tab = tab / 2;
+             std::cout << "\n";
+         }
+     }
+}
 
+template <typename T> //5 - для систематизации печати - заполнение вектора значениями дерева
+void Print_help (Tree_element<T>* Tree, int k, std::vector<T>& Vec, int n, unsigned int key_max)
+{
+    if (Tree != nullptr)
+    {
+        T x;
+        if (k == 200)
+        {
+            x.key = key_max;
+            x.value = Tree -> value;
+            Vec.push_back(x);
+            x.key = key_max - 1;
+            if (Tree -> tree_next_left != nullptr)
+            {
+                x.value = Tree -> tree_next_left -> value;
+            }
+            else
+            {
+                x.value = -1;
+            }
+            Vec.push_back(x);
+            n = key_max - 1;
+            Print_help(Tree -> tree_next_left, 100, Vec, n - 1, key_max);
+            Print_help(Tree -> tree_next_right, 100, Vec, n - 1, key_max);
+        }
+        if (k == 100)
+        {
+            if ((Tree -> tree_next_left != nullptr) || (Tree -> tree_next_right != nullptr))
+            {
+                x.key = n;
+                if (Tree -> tree_next_left != nullptr)
+                {
+                    x.value = Tree -> tree_next_left -> value;
+                }
+                else
+                {
+                    x.value = -1;
+                }
+                Vec.push_back(x);
+                if (Tree -> tree_next_right != nullptr)
+                {
+                    x.value = Tree -> tree_next_right -> value;
+                }
+                else
+                {
+                    x.value = -1;
+                }
+                Vec.push_back(x);
+                Print_help(Tree -> tree_next_left, 100, Vec, n - 1, key_max);
+                Print_help(Tree -> tree_next_right, 100, Vec, n - 1, key_max);
+            }
+            else
+            {
+                x.key = n;
+                x.value = -1;
+                Vec.push_back(x);
+                Vec.push_back(x);
+                Print_help(Tree -> tree_next_left, 101, Vec, n - 1, key_max);
+                Print_help(Tree -> tree_next_right, 101, Vec, n - 1, key_max);
+            }
+        }
+    }
 }
 
 int main()
 {
-
+    //проверка для стандартного типа
+    Tree_element<std::vector>* tree_time = new Tree_element<std::vector>;
+    Constructor (tree_time);
+    for (int i = 0; i < 10; ++i)
+	{
+		tree_time = Push(tree_time, i);
+	}
+	Print(tree_time);
+	std::cout << "\n\n";
+	Erase (tree_time, 10);
+	Erase (tree_time, 2);
+	Erase (tree_time, 4);
+	Print(tree_time);
+	Destructor(tree_time);
     return 0;
 }
